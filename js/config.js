2d78606c86441640174dec265c5eee2bd45e0945
JessKuh5146 <Jessica.Martineztx@gmail.com>
2024-04-20 19:18:30 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId:         'us-east-2_L2cS7v5CA',         // e.g. us-east-2_uXboG5pAb
         userPoolClientId:   '1s0ivdsictq6920532629uupss',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'us-east-2'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
